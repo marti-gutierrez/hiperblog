66b05519c5af1cef73224967159645b12c908282
martingh19 <martigh14@gmail.com>
2021-11-11 19:05:09 -0600

    Agregamos un nuevo nombre y printf

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main(void)
 {
 	int estado;
 	estado = 1;
-	printf("Hola mundo mi nombre es paco \n");
+	printf("Hola mundo mi nombre es paco y jorge \n");
+	printf("El estado actual es %i \n",estado);
 	return 0;
 }
