7c772c22aa5c78d03c459ea17d7433edd8995ccd
martingh19 <martigh14@gmail.com>
2021-11-12 20:35:36 -0600

    preguntamos su edad

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 int main(void)
 {
 	int estado;
-	estado = 1;
 	printf("Hola mundo mi nombre es paco y jorge \n");
-	printf("El estado actual es %i \n",estado);
+	printf("\r Ingrese su edad: ");
+	scanf("%i",&estado);
+	printf("su edad actual es %i \n",estado);
 	return 0;
 }
