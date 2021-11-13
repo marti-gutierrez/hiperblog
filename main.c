e85007b297bb47c1b5098f82792b6fe063f71f25
martingh19 <martigh14@gmail.com>
2021-11-12 20:40:32 -0600

    remplazo una version por otra del main

--- main.c
+++ main.c
@@ -3,9 +3,8 @@
 int main(void)
 {
 	int estado;
-	printf("Hola mundo mi nombre es paco y jorge \n");
-	printf("\r Ingrese su edad: ");
-	scanf("%i",&estado);
-	printf("su edad actual es %i \n",estado);
+	estado = 1;
+	printf("Hola mundo mi nombre es paco \n");
+	printf("Mañana jugaremos al minecraft\n");
 	return 0;
 }
