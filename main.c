d37ca3e334f74d956606686efb3caf3862405d3b
martingh19 <martigh14@gmail.com>
2021-11-23 19:53:27 -0600

    Arreglando error de punto y coma

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(void)
 	estado = 1;
 	printf("Hola mundo mi nombre es paco \n");
 	printf("Hola mi nombre es jorge malvado\n");
-	printf("Hola este es un nuevo archivo de colaboración\n")
+	printf("Hola este es un nuevo archivo de colaboración\n");
 	printf("Mañana jugaremos al minecraft\n");
 	printf("Experimento 2\n");
 	return 0;
